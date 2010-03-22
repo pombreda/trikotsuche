f07ce6dc2b06b920ecfe0be47797ed4373a1c43f
Ramiro Gómez <web@ramiro.org>
2010-03-23 00:00:05 +0100

    changes too long ago

--- lib/zws.php
+++ lib/zws.php
@@ -37,86 +37,85 @@ class Zws extends WebService {
       $this->adspace_id,
       $page,
       $limit
-      
     );
     $items = json_decode($result)->productItems->productItem;
 var_dump($result);
     foreach ($items as $item) {
-      $id = $item->{'@id'};
-var_dump($item, $id);
+var_dump($item);
+      $i = $this->item_prepare($item);
+var_dump($i);die;
     }
   }
   
-//  private function item_prepare($item) {
-//    if (!isset ($item->id)) {
-//      return false;
-//    }
-//    $i['id'] = $item->id;
-//    
-//    if (!isset ($item->name)) {
-//      $i['name'] = ZWS_NO_DATA;
-//    } else {
-//      $i['name'] = check_plain($item->name);
-//    }
-//
-//    if (!isset ($item->program->_)) {
-//      $i['program'] = ZWS_NO_DATA;
-//    } else {
-//      $i['program'] = check_plain($item->program->_);
-//    }
-//
-//    if (!isset ($item->currency)) {
-//      $i['currency'] = ZWS_NO_DATA;
-//    } else {
-//      $i['currency'] = check_plain($item->currency);
-//    }
-//
-//    if (!isset ($item->price)) {
-//      $i['price'] = ZWS_NO_DATA;
-//    } else {
-//      $i['price'] = check_plain($item->price);
-//    }
-//
-//    $i['url'] = '';
-//    $params = $this->params();
-//    $adspace_id = $params['adspace'];
-//    if (!isset ($item->url->adspace->_)) {
-//      if (isset($item->url->adspace)) {
-//        foreach ($item->url->adspace as $a) {
-//          if ($a->id == $adspace_id) {
-//            $i['url'] = check_uri($a->_);
-//            break;
-//          }
-//        }
-//      }
-//    } else {
-//      $i['url'] = check_uri($item->url->adspace->_);
-//    }
-//
-//    if (!isset ($item->manufacturer)) {
-//      $i['manufacturer'] = ZWS_MANUFACTURER;
-//    } else {
-//      $i['manufacturer'] = check_plain($item->manufacturer);
-//    }
-//
-//    if (!isset ($item->image->small)) {
-//      $i['image_small'] = ZWS_IMAGE_SMALL_URL;
-//    } else {
-//      $i['image_small'] = check_uri($item->image->small);
-//    }
-//
-//    if (!isset ($item->image->medium)) {
-//      $i['image_medium'] = ZWS_IMAGE_MEDIUM_URL;
-//    } else {
-//      $i['image_medium'] = check_uri($item->image->medium);
-//    }
-//
-//    if (!isset ($item->image->large)) {
-//      $i['image_large'] = ZWS_IMAGE_LARGE_URL;
-//    } else {
-//      $i['image_large'] = check_uri($item->image->large);
-//    }
-//
-//    return $i;
-//  }
+  private function item_prepare($item) {
+    if (!isset ($item->{'@id'})) {
+      return false;
+    }
+    $i['id'] = $item->{'@id'};
+
+    if (!isset ($item->name)) {
+      return false;
+    } else {
+      $i['name'] = check_plain($item->name);
+    }
+
+    if (!isset ($item->program->{'$'})) {
+      return false;
+    } else {
+      $i['program'] = check_plain($item->program->{'$'});
+    }
+
+    if (!isset ($item->currency)) {
+      return false;
+    } else {
+      $i['currency'] = check_plain($item->currency);
+    }
+
+    if (!isset ($item->price)) {
+      return false;
+    } else {
+      $i['price'] = check_plain($item->price);
+    }
+    
+    if (!isset ($item->image->small)) {
+      return false;
+    } else {
+      $i['image_small'] = check_uri($item->image->small);
+    }
+
+    if (!isset ($item->image->medium)) {
+      return false;
+    } else {
+      $i['image_medium'] = check_uri($item->image->medium);
+    }
+
+    if (!isset ($item->image->large)) {
+      return false;
+    } else {
+      $i['image_large'] = check_uri($item->image->large);
+    }
+
+    $i['url'] = '';
+    $adspace_id = $this->adspace_id;
+    if (isset ($item->trackingLinks->trackingLink->{'@adspaceId'})
+      && $adspace_id == $item->trackingLinks->trackingLink->{'@adspaceId'}
+    ) {
+      $i['url'] = check_uri($item->trackingLinks->trackingLink->ppc);
+    }
+    else {
+      foreach ($item->trackingLinks->trackingLink as $l) {
+        if (isset ($l->{'@adspaceId'}) && $adspace_id == $l->{'@adspaceId'}) {
+          $i['url'] = check_uri($l->ppc);
+        }
+      }
+    }
+
+    if (!isset ($item->manufacturer)) {
+      $i['manufacturer'] = '';
+    } else {
+      $i['manufacturer'] = check_plain($item->manufacturer);
+    }
+
+    return $i;
+  }
 }
---
+++ static/js/zws.js
@@ -0,0 +1,60 @@
+var Zws = {};
+
+/**
+ * function to add scripts to html header
+ */
+Zws.addScript = function(url) {
+  var script = document.createElement('script');
+  // preventing caching of the Web Service call by adding timestamp
+  script.src = url + '&t=' + new Date().getMinutes(); 
+  script.type = 'text/javascript';
+  document.getElementsByTagName('head')[0].appendChild(script);
+}
+
+/**
+ * zanox product search
+ * 
+ * @see http://wiki.zanox.com/en/Products_Resource
+ */
+Zws.productSearch = function(version, applicationid, adspace, region, programs, minPrice, maxPrice, category, page, items, q, callback) {
+  var url = 'http://api.zanox.com/json/' + version + '/products?applicationid=' + applicationid;
+  if (adspace) url += '&adspace=' + adspace;
+  if (region) url += '&region=' + region;
+  if (programs) url += '&programs=' + programs;
+  if (category) url += '&category=' + category;
+  if (page) url += '&page=' + page;
+  if (items) url += '&items=' + items;
+  if (minPrice) url += '&minPrice=' + minPrice;
+  if (maxPrice) url += '&maxPrice=' + maxPrice;
+  if (callback) url += '&callback=' + callback;
+  if (q) url += '&q=' + q;
+  Zws.addScript(url);
+  console.log(url);
+}
+
+Zws.handler = function(data) {
+  if (data.productsResult) {
+    var items = data.productsResult.productItem;
+    // ID of a UL HTML element
+    var zxAdList = document.getElementById('zxAdList');
+    for (var i = 0; i < items.length; i++) {
+      var item = items[i];
+      var link = '';
+      if (item.url.adspace instanceof Array) {
+        // select tracking link related to the first Adspace
+        link = item.url.adspace[0].$;
+      }
+      else {
+        link = item.url.adspace.$;
+      }
+      var content = item.name + ' bei ' + item.program.$ + ' für ' + item.price + ' ' + item.currency;
+      Zws.renderItem(content, link, zxAdList);
+    }
+  }
+}
+
+Zws.renderItem = function(content, link, container) {
+  var li = document.createElement("li");
+  li.innerHTML = '<a href="' + link + '">' + content + '</a>';  
+  container.appendChild(li);
+}
