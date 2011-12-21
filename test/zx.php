3df065ee38dd7109a0c91c87af60f1595ce6ac99
Ramiro Gomez <web@ramiro.org>
2011-12-21 23:12:18 +0100

    make test work

--- test/zx.php
+++ test/zx.php
@@ -2,17 +2,29 @@
 error_reporting(E_NOTICE | E_WARNING | E_ERROR);
 header('Content-Type: text/plain');
 include_once('../conf/settings.php');
-include_once('../lib/3rd/zxapiclient_2009_07_01/ApiClient.php');
+include_once('../lib/3rd/zanox/client/ApiClient.php');
 
 #$params = array('region' => $zws_region, 'adspace' => $zws_adspace_id);
-$zx = ApiClient::factory(PROTOCOL_JSON, VERSION_2009_07_01);
+$zx = ApiClient::factory(PROTOCOL_JSON, VERSION_2011_03_01);
 $zx->setConnectId($zws_connect_id);
 $zx->setSecretKey($zws_secret_key);
 $zx->setPublicKey($zws_public_key);
 
-#$result = $zx->searchProducts('trikot', $params, 0, 10);
-$result = json_decode($zx->getProgramsByAdspace(961381, 0, 50));
-foreach ($result->programItems->programItem as $i) {
-  printf("%d\t%s\n", $i->{'@id'}, $i->name);
+foreach (range(0,2) as $idx)
+    var_dump(search($zx, $idx));
+
+function search($zx, $page) {
+  return $zx->searchProducts(
+    'trikot',
+    'phrase',
+  null,
+  null,
+  array(),
+  true,
+  0,
+  null,
+  994828,
+  $page,
+  10
+  );
 }
-#var_dump($result);
